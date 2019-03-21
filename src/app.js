06f2aeac0cc53d1e06111318599ccf70eaf00a67
andori <falcsmike7@gmail.com>
2019-03-20 20:22:14 -0400

    react components

--- src/app.js
+++ src/app.js
@@ -1,61 +1,51 @@
-console.log('App.js is running!');
-
-const app = {
-  title: 'Indecision App',
-  subtitle: 'Put your life in the hands of a computer',
-  options: []
-};
-
-const onFormSubmit = (e) => {
-  e.preventDefault();
-
-  const option = e.target.elements.option.value;
-
-  if (option) {
-    app.options.push(option);
-    e.target.elements.option.value = '';
-    render();
+class Header extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Indecision</h1>
+        <h2>Put your life in the hands of a computer</h2>
+      </div>
+    )
   }
-};
+}
 
-const onRemoveAll = () => {
-  app.options = [];
-  render();
-};
+class Action extends React.Component {
+  render() {
+    return (
+      <div>
+        <button>What should I do?</button>
+      </div>
+    )
+  }
+}
 
-const appRoot = document.getElementById('app');
+class Option extends React.Component {
+  render() {
+    return (
+      <div>
+        <p>Your options here.</p>
+      </div>
+    )
+  }
+}
 
-const numbers = [55, 101, 1000];
+class AddOption extends React.Component {
+  render() {
+    return (
+      <div>
+        <p>Add options here.</p>
+      </div>
+    )
+  }
+}
 
-const render = () => {
-  const template = (
-    <div>
-      <h1>{app.title}</h1>
-      {app.subtitle && <p>{app.subtitle}</p>}
-      <p>{app.options.length > 0 ? 'Here are your options' : 'No options'}</p>
-      <p>{app.options.length}</p>
-      <button onClick={onRemoveAll}>Remove All</button>
-      {
-        numbers.map((number) => {
-          return <p key={number}>Number: {number}</p>
-        })
-      }
-      <ol>
-      {
-        app.options.map((option) => {
-          return <li key={option}>{option}</li>
-        })
-      }
-      </ol>
-      
-      <form onSubmit={onFormSubmit}>
-        <input type="text" name="option" />
-        <button>Add Option</button>
-      </form>
-    </div>
-  );
+const jsx = (
+  <div>
+    <Header />
+    <Action />
+    <Option />
+    <AddOption />
+  </div>
+)
 
-  ReactDOM.render(template, appRoot);
-};
-
-render();
+ReactDOM.render(jsx, document.getElementById('app'));
